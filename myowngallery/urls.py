18105030afaf59bd9d647b3d613599a6cbab35ce
Taylor Sando <taylor_sando@hotmail.com>
2012-08-11 09:50:44 -0500

    Added another view to test urls config

--- myowngallery/urls.py
+++ myowngallery/urls.py
@@ -14,5 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^goodbye', 'temp.views.goodbye'),
     url(r'^.*?', 'temp.views.hello'),
+    
 )
--- temp/views.py
+++ temp/views.py
@@ -2,5 +2,8 @@
 
 from django.http import HttpResponse
 
+def goodbye(request):
+    return HttpResponse("Goodbye, Cruel World")
+
 def hello(request):
     return HttpResponse("Hello World")
