b055b37492cc4ce00221260e652a9b9b7ce7aac6
Taylor Sando <taylor_sando@hotmail.com>
2012-08-11 10:57:47 -0500

    Testing template system1

--- temp/views.py
+++ temp/views.py
@@ -3,10 +3,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
+import myowngallery.settings
+
 def goodbye(request):
     
     return render_to_response("temp.html", 
                               {'var1': "Hello, fine world"})
 
 def hello(request):
+    return HttpResponse(myowngallery.settings.SITE_ROOT)
     return HttpResponse("Hello World")
