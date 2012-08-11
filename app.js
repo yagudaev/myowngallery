73570da06b3654d3ef7e9ab79a6abb402b60c033
Taylor Sando <taylor_sando@hotmail.com>
2012-08-11 18:14:25 -0500

    Added the login page

--- app.js
+++ app.js
@@ -49,6 +49,8 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/signup', routes.showSignup);
 app.post('/signup', routes.processSignup);
+app.get('/login', routes.login);
+app.post('/userpage', routes.userPage);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ exports.index = function(req, res){
 };
 
 exports.showSignup = function(req, res) {
-	res.render('signup', {title: 'Signup', errors: [] })
-}
+	res.render('signup', {title: 'Signup', errors: [] });
+};
 
 exports.processSignup = function(req, res) {
 	var user = new db.models.User({
@@ -33,6 +33,40 @@ exports.processSignup = function(req, res) {
 		}
 
 		res.render('signup', {title: 'Signup Successful', errors: errors });
-	})
+	});
 	
-}
+};
+
+exports.login = function(req, res) {
+    
+  res.render('login', {title: 'Login', errors: []});  
+};
+
+exports.userPage = function(req, res) {
+    
+    var reqUserName = req.body.txtUserName;
+    var reqPassword = req.body.txtPassword;
+
+    db.models.User.findOne({username: reqUserName,
+			   password: reqPassword}, function(err, user){ 
+					   
+					   if(err) {
+					       console.log(err);
+					       res.send(500, "error");					       
+					       return;
+					   }
+
+					   if(user) {
+					       res.render('userpage', {title: 'Title', errors: [],
+								      userName: user.username,
+								      firstName: user.name.first,
+								      lastName: user.name.last});
+					       return;
+					   } else {
+					       res.render('login', {title: 'Title', errors: ['Username/Password invalid']});
+					       return;
+					   }
+				       });
+
+    
+};
