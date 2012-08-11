04852c52e07b99033358f06d7972f05c10588481
Taylor Sando <taylor_sando@hotmail.com>
2012-08-11 11:25:13 -0500

    Added gallery

---
+++ gallery/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+def goodbye(request):
+    
+    return render_to_response("temp.html", 
+                              {'var1': "Hello, fine world"})
+
+def hello(request):
+    
+    return HttpResponse("Hello World")
+
+
+
