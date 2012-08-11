77b60e3caa2f7056b37ceaa3485e17e5568666a9
Taylor Sando <taylor_sando@hotmail.com>
2012-08-11 03:12:46 -0500

    Added temp app for models

--- myowngallery/settings.py
+++ myowngallery/settings.py
@@ -13,6 +13,7 @@ MANAGERS = ADMINS
 
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -108,7 +109,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gunicorn'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
