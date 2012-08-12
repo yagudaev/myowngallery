8913c740c88bb3da689a1afbb92b7034efdece58
Taylor Sando <taylor_sando@hotmail.com>
2012-08-12 09:00:38 -0500

    Added buyer section

---
+++ public/javascripts/buyer.js
@@ -0,0 +1,9 @@
+(function ($) {
+
+	$(document).ready(function() {
+			      
+			      $('#featuredPics1').append("<img src='images/feature1-a.jpg' width='300' height='300' />");
+			      $('#featuredPics2').append("<img src='images/feature1-a.jpg' width='300' height='300' />");
+			      $('#featuredPics3').append("<img src='images/feature1-a.jpg' width='300' height='300' />");
+			  });
+}(jQuery));
